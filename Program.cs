40d2a591e263ddce7799abc18ca10aa9177a8e50
contidavide-sudo <conti.davide@franchettisalviani.net>
2025-10-21 20:47:05 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace fattoriale_2._0
+{
+    internal class Program
+    {
+        static bool Pari(int n)
+        {
+            if (n % 2 == 0)
+            {
+                return true;
+            }
+            else 
+            { 
+                 return false;
+            }
+        }
+        static int Fattoriale(int n)
+        {
+            int m = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                m = m * i;
+
+            }
+
+            return m;
+        }
+        static void Main(string[] args)
+        {
+            int num = 10, cont = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (Pari(i) == true)
+                {
+                    int fat = Fattoriale(i);
+                    cont = fat + cont;
+                }
+            }
+
+            Console.WriteLine(cont);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# fattoriale_2.0
+Esercizio sulle funzioni fattoriale_2.0
